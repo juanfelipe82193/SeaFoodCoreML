d9149d7b179f2dcc65d05042b37b6eb3650a9a0a
Juan Felipe Zorrilla <juan19_93@hotmail.com>
2022-08-10 19:37:39 -0500

    Initial Commit

---
+++ SeaFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeaFood
+//
+//  Created by Juan Felipe Zorrilla Ocampo on 10/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
