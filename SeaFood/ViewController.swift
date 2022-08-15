04a25c942499f8bbb57fca174d39c859ccf3e263
Juan Felipe Zorrilla <juan19_93@hotmail.com>
2022-08-15 17:34:00 -0500

    Project ended

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -13,15 +13,94 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @IBOutlet weak var imageView: UIImageView!
     
+//    let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        imagePicker.delegate = self
+//        imagePicker.sourceType = .camera
+//        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let userPickedImage = info[.originalImage] as? UIImage, let ciimage = CIImage(image: userPickedImage) else {
+            fatalError("Could not convert picked image to UIImage or CIImage")
+        }
         
+        imageView.image = userPickedImage
+        detect(image: ciimage)
+        
+        dismiss(animated: true)
     }
 
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: MLModel(contentsOf: Inceptionv3.urlOfModelInThisBundle)) else {
+            fatalError("Loading CoreML Model Failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image.")
+            }
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
+    
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         
+//        present(imagePicker, animated: true, completion: nil)
+        openCamera()
+        
     }
+    
+    @IBAction func libraryTapped(_ sender: UIBarButtonItem) {
+        openLibrary()
+    }
+    
+    func openCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera
+            imagePicker.allowsEditing = false
+            present(imagePicker, animated: true)
+        } else {
+            let alert = UIAlertController(title: "Warning", message: "Camera it's not enabled", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true)
+        }
+    }
+    
+    func openLibrary() {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = false
+            imagePicker.sourceType = .photoLibrary
+            present(imagePicker, animated: true)
+        } else {
+            let alert = UIAlertController(title: "Warning", message: "You don't have permission to access gallery", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+
 }
+
 
